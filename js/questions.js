da3617f114470f68d327ed50be369e424c1ea910
Ganiti <85805663+Ganiti@users.noreply.github.com>
2021-12-03 21:15:33 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What was Australia's former name?",
+    answer: "New Holland",
+    options: [
+      "Bosnia",
+      "New Amsterdam",
+      "New Holland",
+      "Andorra"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of the medical condition where people randomly develop a foreign accent",
+    answer: "Foreign Accent Syndrome",
+    options: [
+      "Foreign Accent Syndrome",
+      "frenchly flu",
+      "foreign copy flu",
+      "Insomnia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Cabbage contains what percentage of water?",
+    answer: "91%",
+    options: [
+      "2%",
+      "91%",
+      "32%",
+      "52%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "According to statistics you ll live longest in which US state",
+    answer: "Hawaii",
+    options: [
+      "Hawaii",
+      "New York",
+      "Maryland",
+      "California"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the national animal of Scotland ",
+    answer: "Unicorn",
+    options: [
+      "Horse",
+      "Mermaid",
+      "Horse",
+      "Unicorn"
+    ]
+  },
+  
+];
